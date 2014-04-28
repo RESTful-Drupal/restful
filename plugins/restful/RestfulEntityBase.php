53fef324c05dd8461d4c947d2b4e76fa72e75f41
Snir <snir@gizra.com>
2014-04-28 16:44:53 +0300

    Set a default value and condition fix

--- plugins/restful/RestfulEntityBase.php
+++ plugins/restful/RestfulEntityBase.php
@@ -287,6 +287,7 @@ abstract class RestfulEntityBase implements RestfulEntityInterface {
       $info += array(
         'wrapper_method' => 'value',
         'wrapper_method_on_entity' => FALSE,
+        'sub_property' => FALSE,
       );
 
       $property = $info['property'];
@@ -297,7 +298,7 @@ abstract class RestfulEntityBase implements RestfulEntityInterface {
           throw new Exception(format_string('Property @property does not exist.', array('@property' => $property)));
         }
 
-        if (!empty($info['sub_property'])) {
+        if (!$info['sub_property']) {
           $sub_wrapper =  $wrapper->{$property}->{$info['sub_property']};
         }
         else {
@@ -311,7 +312,7 @@ abstract class RestfulEntityBase implements RestfulEntityInterface {
       else {
         $sub_wrapper = $info['wrapper_method_on_entity'] ? $wrapper : $wrapper->{$property};
 
-        if (!empty($info['sub_property'])) {
+        if (!$info['sub_property']) {
           $sub_wrapper = $sub_wrapper->{$info['sub_property']};
         }
 
