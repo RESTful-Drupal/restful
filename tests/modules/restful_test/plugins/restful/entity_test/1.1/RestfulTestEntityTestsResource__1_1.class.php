65b4549ebcfd3b2afdafd47361c2f1412122a69a
Amitai Burstein <amitai@gizra.com>
2014-05-01 21:11:02 +0300

    More.

--- tests/modules/restful_test/plugins/restful/entity_test/1.1/RestfulTestEntityTestsResource__1_1.class.php
+++ tests/modules/restful_test/plugins/restful/entity_test/1.1/RestfulTestEntityTestsResource__1_1.class.php
@@ -2,8 +2,21 @@
 
 /**
  * @file
- * Contains RestfulExampleArticlesResource__1_1.
+ * Contains RestfulTestEntityTestsResource__1_1.
  */
 
-class RestfulExampleArticlesResource__1_1 extends RestfulTestEntityTestsResource {
+class RestfulTestEntityTestsResource__1_1 extends RestfulTestEntityTestsResource {
+
+  /**
+   * Overrides RestfulExampleArticlesResource::getPublicFields().
+   */
+  public function getPublicFields() {
+    $public_fields = parent::getPublicFields();
+
+    $public_fields['text_single'] = array(
+      'property' => 'text_single',
+      'sub_property' => 'value',
+    );
+    return $public_fields;
+  }
 }
