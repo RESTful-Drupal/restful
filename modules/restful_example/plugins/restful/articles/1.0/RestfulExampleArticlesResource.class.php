ab568de997fa67b741cd51772496211c73938392
Amitaibu <amitai@gizra.com>
2014-05-05 11:47:20 +0300

    REmove debug

--- modules/restful_example/plugins/restful/articles/1.0/RestfulExampleArticlesResource.class.php
+++ modules/restful_example/plugins/restful/articles/1.0/RestfulExampleArticlesResource.class.php
@@ -6,15 +6,4 @@
  */
 
 class RestfulExampleArticlesResource extends RestfulEntityBaseNode {
-
-  public function getPublicFields() {
-    $public_fields = parent::getPublicFields();
-    $public_fields['er'] = array(
-      'property' => 'field_er',
-      'resource' => array(
-        'article' => 'articles',
-      ),
-    );
-    return $public_fields;
-  }
 }
