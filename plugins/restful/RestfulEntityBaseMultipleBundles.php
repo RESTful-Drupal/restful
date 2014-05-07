08d1a92c9d9fc540d533b13b65987bb7adc6f271
Snir <snir@gizra.com>
2014-05-07 09:59:33 +0300

    Multiple bundles support

---
+++ plugins/restful/RestfulEntityBaseMultipleBundles.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @file
+ * Contains RestfulEntityBaseMultipleBundles.
+ */
+
+class RestfulEntityBaseMultipleBundles extends RestfulEntityBase {
+
+  /**
+   * Define the bundles to expose to the API.
+   *
+   * Bundle => Resource
+   */
+  protected $bundles = array();
+
+  protected $controllers = array(
+    '' => array(
+      // GET returns a list of entities.
+      'get' => 'getList',
+    ),
+  );
+
+  public function __construct($plugin) {
+    parent::__construct();
+
+    $this->bundles = $plugin['bundles'];
+  }
+
+  /**
+   * Return the bundles.
+   *
+   * @return array
+   *  An array of the exposed bundles.
+   */
+  protected function getBundles() {
+    return $this->bundles;
+  }
+
+  /**
+   * Get a list of entities.
+   *
+   * @param null $request
+   *   (optional) The request.
+   * @param null $account
+   *   (optional) The user object.
+   *
+   * @return array
+   *   Array of entities, as passed to RestfulEntityBase::getView().
+   *
+   * @throws RestfulBadRequestException
+   */
+  public function getList($request, $account) {
+    $handlers = array();
+    $return = array();
+    foreach ($this->getBundles() as $bundle => $resource) {
+      $handlers[$bundle] = restful_get_restful_handler($resource);
+
+      $results = $handlers[$bundle]->getList($request, $account);
+      $return['list'][$bundle] = $results['list'];
+    }
+
+    return $return;
+  }
+
+}
