fa4df8f988fd461ad479e85b656374beb4d5b334
Snir <snir@gizra.com>
2014-05-18 12:08:19 +0300

    Added base taxonomy class

---
+++ plugins/restful/RestfulEntityBaseTaxonomy.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/**
+ * @file
+ * Contains RestfulEntityBaseTaxonomy.
+ */
+
+/**
+ * A base implementation for "Taxonomy" entity type.
+ */
+class RestfulEntityBaseTaxonomy extends RestfulEntityBase {
+
+}
