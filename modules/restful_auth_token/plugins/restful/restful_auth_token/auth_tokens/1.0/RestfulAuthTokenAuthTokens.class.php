6a9c4fe56611717490c64849b416a15d743129ce
Amitaibu <amitai@gizra.com>
2014-05-25 16:52:09 +0300

    Add badic auth.

--- modules/restful_auth_token/plugins/restful/restful_auth_token/auth_tokens/1.0/RestfulAuthTokenAuthTokens.class.php
+++ modules/restful_auth_token/plugins/restful/restful_auth_token/auth_tokens/1.0/RestfulAuthTokenAuthTokens.class.php
@@ -6,4 +6,26 @@
  */
 
 class RestfulAuthTokenAuthTokens extends RestfulEntityBaseNode {
+
+  /**
+   * Nested array that provides information about what method to call for each
+   * route pattern.
+   *
+   * @var array $controllers
+   */
+  protected $controllers = array(
+    '' => array(
+      // GET returns a list of entities.
+      'get' => 'createAndGetToken',
+    ),
+  );
+
+  public function createAndGetToken() {
+    $account = $this->getAccount();
+
+    // Check if there is a token that did not expire yet.
+
+    // Return the token.
+
+  }
 }
