81f47ee7690373e136c87f5b1f78a29f2545cf9d
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2014-05-25 11:17:42 +0200

    Return early.

--- plugins/authentication/RestfulAuthenticationBasic.class.php
+++ plugins/authentication/RestfulAuthenticationBasic.class.php
@@ -33,41 +33,38 @@ class RestfulAuthenticationBasic extends RestfulAuthenticationBase implements Re
     if (!flood_is_allowed('failed_login_attempt_ip', variable_get('user_failed_login_ip_limit', 50), variable_get('user_failed_login_ip_window', 3600))) {
       throw new RestfulFloodException(format_string('Rejected by ip flood control.'));
     }
-    $uid = db_query_range("SELECT uid FROM {users} WHERE name = :name AND status = 1", 0, 1, array(':name' => $username))->fetchField();
-    if ($uid) {
-      if (variable_get('user_failed_login_identifier_uid_only', FALSE)) {
-        // Register flood events based on the uid only, so they apply for any
-        // IP address. This is the most secure option.
-        $identifier = $uid;
-      }
-      else {
-        // The default identifier is a combination of uid and IP address. This
-        // is less secure but more resistant to denial-of-service attacks that
-        // could lock out all users with public user names.
-        $identifier = $uid . '-' . ip_address();
-      }
-
-      // Don't allow login if the limit for this user has been reached.
-      // Default is to allow 5 failed attempts every 6 hours.
-      if (flood_is_allowed('failed_login_attempt_user', variable_get('user_failed_login_user_limit', 5), variable_get('user_failed_login_user_window', 21600), $identifier)) {
-        // We are not limited by flood control, so try to authenticate.
-        if ($uid = user_authenticate($username, $password)) {
-          // Clear the user based flood control.
-          flood_clear_event('failed_login_attempt_user', $identifier);
-          return user_load($uid);
-        }
-        else {
-          flood_register_event('failed_login_attempt_user', variable_get('user_failed_login_user_window', 3600), $identifier);
-        }
-      }
-      else {
-        flood_register_event('failed_login_attempt_user', variable_get('user_failed_login_user_window', 3600), $identifier);
-        throw new RestfulFloodException(format_string('Rejected by user flood control.'));
-      }
+    if (!$uid = db_query_range("SELECT uid FROM {users} WHERE name = :name AND status = 1", 0, 1, array(':name' => $username))->fetchField()) {
+      // Always register an IP-based failed login event.
+      flood_register_event('failed_login_attempt_ip', variable_get('user_failed_login_ip_window', 3600), ip_address());
+      return;
+    }
+    if (variable_get('user_failed_login_identifier_uid_only', FALSE)) {
+      // Register flood events based on the uid only, so they apply for any
+      // IP address. This is the most secure option.
+      $identifier = $uid;
+    }
+    else {
+      // The default identifier is a combination of uid and IP address. This
+      // is less secure but more resistant to denial-of-service attacks that
+      // could lock out all users with public user names.
+      $identifier = $uid . '-' . ip_address();
     }
 
-    // Always register an IP-based failed login event.
-    flood_register_event('failed_login_attempt_ip', variable_get('user_failed_login_ip_window', 3600), ip_address());
-    return NULL;
+    // Don't allow login if the limit for this user has been reached.
+    // Default is to allow 5 failed attempts every 6 hours.
+    if (flood_is_allowed('failed_login_attempt_user', variable_get('user_failed_login_user_limit', 5), variable_get('user_failed_login_user_window', 21600), $identifier)) {
+      // We are not limited by flood control, so try to authenticate.
+      if ($uid = user_authenticate($username, $password)) {
+        // Clear the user based flood control.
+        flood_clear_event('failed_login_attempt_user', $identifier);
+        return user_load($uid);
+      }
+      flood_register_event('failed_login_attempt_user', variable_get('user_failed_login_user_window', 3600), $identifier);
+    }
+    else {
+      flood_register_event('failed_login_attempt_user', variable_get('user_failed_login_user_window', 3600), $identifier);
+      throw new RestfulFloodException(format_string('Rejected by user flood control.'));
+    }
   }
+
 }
