beba0259fa43e4efb2c81e46ca957c9401a6e4c3
Snir Segal <snir.segal@gmail.com>
2014-06-14 13:28:10 +0300

    Doc typo

--- plugins/restful/RestfulEntityBase.php
+++ plugins/restful/RestfulEntityBase.php
@@ -713,7 +713,7 @@ abstract class RestfulEntityBase implements RestfulEntityInterface {
   /**
    * Update an entity using PUT.
    *
-   * Non existing properties are assumed to be euqal to NULL.
+   * Non existing properties are assumed to be equal to NULL.
    *
    * @param $entity_id
    *   The entity ID.
