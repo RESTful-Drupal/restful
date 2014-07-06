bf57f6dc01cc7fa249dcb241902e1dd5f8a68370
Amitaibu <amitai@gizra.com>
2014-07-06 15:10:32 +0300

    Add body.

--- modules/restful_example/plugins/restful/node/articles/1.0/RestfulExampleArticlesResource.class.php
+++ modules/restful_example/plugins/restful/node/articles/1.0/RestfulExampleArticlesResource.class.php
@@ -6,4 +6,18 @@
  */
 
 class RestfulExampleArticlesResource extends RestfulEntityBaseNode {
+
+  /**
+   * Overrides RestfulExampleArticlesResource::getPublicFields().
+   */
+  public function getPublicFields() {
+    $public_fields = parent::getPublicFields();
+
+    $public_fields['body'] = array(
+      'property' => 'body',
+      'sub_property' => 'value',
+    );
+
+    return $public_fields;
+  }
 }
