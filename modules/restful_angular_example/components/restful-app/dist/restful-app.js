3d6fa9a073a383ad35724e83b68fa02584a894db
Amitai Burstein <amitai@gizra.com>
2014-07-22 23:16:51 +0300

    More work.

--- modules/restful_angular_example/components/restful-app/dist/restful-app.js
+++ modules/restful_angular_example/components/restful-app/dist/restful-app.js
@@ -64,6 +64,8 @@ angular.module('restfulApp', [
 angular.module('restfulApp')
   .controller('MainCtrl', function($scope, DrupalSettings, ArticlesResource, FileUpload, $http, $log) {
     $scope.data = DrupalSettings.getData('article');
+    $scope.data.label = 'yes',
+    $scope.data.body = 'Drupal stuff',
     $scope.serverSide = {};
 
     /**
@@ -90,24 +92,25 @@ angular.module('restfulApp')
     };
 
     /**
-     * Create a new term.
-     *
-     * @param term
-     *
-     * @returns {{text: *, id: number}}
+     * Submit form (even if not validated via client).
      */
-    $scope.createSearchChoice = function (term) {
-      return {
-        text: term,
-        id: -10
-      };
-    };
+    $scope.submitForm = function() {
+      // Prepare the tags, by removing the IDs that are not integer, so it will
+      // use POST to create them.
+      var submitData = $scope.data;
+      var tags = [];
+      angular.forEach(data.tags, function (term, index) {
+        tags[index] = {};
+        tags[index].label = term.text;
+        if (term.id === parseInt(term.id)) {
+          tags[index].id = term.id;
+        }
+      });
 
-    /**
-     * Submit form (even if not valildated via client).
-     */
-    $scope.submitForm = function(){
-      ArticlesResource.createArticle($scope.data)
+      submitData.tags = tags;
+      $log.log(submitData);
+
+      ArticlesResource.createArticle(submitData)
         .success(function(data, status, headers, config) {
           $scope.serverSide.data = data;
           $scope.serverSide.status = status;
