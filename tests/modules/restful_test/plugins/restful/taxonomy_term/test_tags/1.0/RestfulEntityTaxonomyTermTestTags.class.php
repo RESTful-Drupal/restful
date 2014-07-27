a0f25e89e9f28607a937c370535d27fc6606a08b
Amitaibu <amitai@gizra.com>
2014-07-27 16:33:00 +0300

    term test

---
+++ tests/modules/restful_test/plugins/restful/taxonomy_term/test_tags/1.0/RestfulEntityTaxonomyTermTestTags.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Contains \RestfulEntityTaxonomyTermTestTags.
+ */
+
+class RestfulEntityTaxonomyTermTestTags extends \RestfulEntityBaseTaxonomyTerm {
+
+  /**
+   * Overrides \RestfulEntityBaseTaxonomyTerm::checkEntityAccess().
+   *
+   * Allow access to create "Tags" resource for privileged users, as
+   * we can't use entity_access() since entity_metadata_taxonomy_access()
+   * denies it for a non-admin user.
+   */
+  protected function checkEntityAccess($op, $entity_type, $entity) {
+    $account = $this->getAccount();
+    return user_access('create article content', $account);
+  }
+}
