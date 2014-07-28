643285f300be5e111c59d1d23346bcc0afbcf3c6
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2014-07-28 12:11:08 +0200

    Issue #105: Add missing files.

---
+++ plugins/rate_limit/RestfulRateLimitGlobal.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Contains \RestfulRateLimitGlobal
+ */
+
+class RestfulRateLimitGlobal implements \RestfulRateLimitInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isRequestedEvent($request = NULL) {
+    // Only track the global limit for the current user if the variable is on.
+    return variable_get('restful_global_rate_limit', 0) > 0;
+  }
+}
