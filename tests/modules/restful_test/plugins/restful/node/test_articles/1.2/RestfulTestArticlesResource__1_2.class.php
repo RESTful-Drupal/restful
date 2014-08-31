d920c7010754c035e5858f56a3ea23702d8ccfd3
Amitai Burstein <amitai@gizra.com>
2014-08-31 22:28:12 +0300

    Correct handler

--- tests/modules/restful_test/plugins/restful/node/test_articles/1.2/RestfulTestArticlesResource__1_2.class.php
+++ tests/modules/restful_test/plugins/restful/node/test_articles/1.2/RestfulTestArticlesResource__1_2.class.php
@@ -18,19 +18,37 @@ class RestfulTestArticlesResource__1_2 extends RestfulEntityBaseNode {
       'sub_property' => 'value',
     );
 
-    $public_fields['entity_reference_single'] = array(
-      'property' => 'entity_reference_single',
-      'resource' => array(
-        'article' => 'test_articles',
-      ),
-    );
+    // By checking that the field exists, we allow re-using this class on
+    // different tests, where different fields exist.
+    if (field_info_field('entity_reference_single')) {
+      $public_fields['entity_reference_single'] = array(
+        'property' => 'entity_reference_single',
+        'resource' => array(
+          'article' => 'test_articles',
+        ),
+      );
+    }
 
-    $public_fields['entity_reference_multiple'] = array(
-      'property' => 'entity_reference_multiple',
-      'resource' => array(
-        'article' => 'test_articles',
-      ),
-    );
+    if (field_info_field('entity_reference_multiple')) {
+      $public_fields['entity_reference_multiple'] = array(
+        'property' => 'entity_reference_multiple',
+        'resource' => array(
+          'article' => 'test_articles',
+        ),
+      );
+    }
+
+    if (field_info_field('integer_single')) {
+      $public_fields['integer_single'] = array(
+        'property' => 'integer_single',
+      );
+    }
+
+    if (field_info_field('integer_multiple')) {
+      $public_fields['integer_multiple'] = array(
+        'property' => 'integer_multiple',
+      );
+    }
 
 
     return $public_fields;
