ff8654049824e1bfb6654ef587629c8987491fa1
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2014-09-01 12:04:04 +0200

    Last test fixes.

--- plugins/restful/RestfulFilesUpload.php
+++ plugins/restful/RestfulFilesUpload.php
@@ -28,7 +28,9 @@ class RestfulFilesUpload extends \RestfulEntityBase {
   public function __construct($plugin, \RestfulAuthenticationManager $auth_manager = NULL, \DrupalCacheInterface $cache_controller = NULL) {
     parent::__construct($plugin, $auth_manager, $cache_controller);
 
-    $options = $this->getPluginInfo('options');
+    if (!$options = $this->getPluginInfo('options')) {
+      $options = array();
+    }
 
     $default_values = array(
       'validators' => array(
