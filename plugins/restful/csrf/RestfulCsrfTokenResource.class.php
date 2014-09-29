e9b71b2fa6567d7d6eba97177cbaef9287611a5a
Amitai Burstein <amitai@gizra.com>
2014-09-29 09:06:25 +0300

    more work

--- plugins/restful/csrf/RestfulCsrfTokenResource.class.php
+++ plugins/restful/csrf/RestfulCsrfTokenResource.class.php
@@ -12,7 +12,7 @@ class RestfulCsrfTokenResource extends RestfulBase {
    */
   public function publicFieldsInfo() {
     $public_fields = array();
-    $public_fields['csrf_token'] = array(
+    $public_fields['X-CSRF-Token'] = array(
       'callback' => array($this, 'getCsrfToken'),
     );
     return $public_fields;
@@ -20,10 +20,10 @@ class RestfulCsrfTokenResource extends RestfulBase {
 
   /**
    * Value callback; Return the CSRF token.
-   * 
+   *
    * @return array
    */
   protected function getCsrfToken() {
-    return restful_csrf_session_token();
+    return drupal_get_token(\RestfulInterface::TOKEN_VALUE);
   }
 }
