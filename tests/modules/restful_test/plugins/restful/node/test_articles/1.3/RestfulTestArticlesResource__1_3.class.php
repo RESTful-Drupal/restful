bc6701dcb3065b24bf27cf7b19328096a8ac0dab
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2014-10-06 11:08:46 +0200

    Issue #141: Allow more options per endpoint.

---
+++ tests/modules/restful_test/plugins/restful/node/test_articles/1.3/RestfulTestArticlesResource__1_3.class.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file
+ * Contains RestfulExampleArticlesResource__1_3.
+ */
+
+class RestfulTestArticlesResource__1_3 extends RestfulEntityBaseNode {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function controllersInfo() {
+    $info = parent::controllersInfo();
+    $info['^(\d+,)*\d+$'][\RestfulInterface::GET] = array(
+      'callback' => 'viewEntities',
+      'access callback' => 'accessViewEntityFalse',
+    );
+    $info['^(\d+,)*\d+$'][\RestfulInterface::HEAD] = array(
+      'callback' => 'viewEntities',
+      'access callback' => 'accessViewEntityTrue',
+    );
+    return $info;
+  }
+
+  /**
+   * Custom access callback for the GET method.
+   *
+   * @return boolean
+   *   TRUE for access granted, FALSE otherwise.
+   */
+  protected function accessViewEntityFalse() {
+    return FALSE;
+  }
+
+  /**
+   * Custom access callback for the GET method.
+   *
+   * @return boolean
+   *   TRUE for access granted, FALSE otherwise.
+   */
+  protected function accessViewEntityTrue() {
+    return TRUE;
+  }
+
+}
