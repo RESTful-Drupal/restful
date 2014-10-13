c2e34259397c1b6e5d4af566172b75d218c7728c
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2014-10-13 14:37:19 +0300

    Allow passing the resource version number under 'resource' property.

--- plugins/restful/RestfulBase.php
+++ plugins/restful/RestfulBase.php
@@ -903,4 +903,36 @@ abstract class RestfulBase implements RestfulInterface {
     return $formatter_names;
   }
 
+  /**
+   * Return the last version for a given resource.
+   *
+   * @param string $resource_name
+   *   The name of the resource.
+   * @param int $major_version
+   *   Get the last version for this major version. If NULL the last major
+   *   version for the resource will be used.
+   *
+   * @return array
+   *   Array containing the major_version and minor_version.
+   */
+  public static function getResourceLastVersion($resource_name, $major_version = NULL) {
+    $resources = array();
+    // Get all the resources corresponding to the resource name.
+    foreach (restful_get_restful_plugins() as $resource) {
+      if ($resource['resource'] != $resource_name || (isset($major_version) && $resource['major_version'] != $major_version)) {
+        continue;
+      }
+      $resources[$resource['major_version']][$resource['minor_version']] = $resource;
+    }
+    // Sort based on the major version.
+    ksort($resources, SORT_NUMERIC);
+    // Get a list of resources for the latest major version.
+    $resources = end($resources);
+    // Sort based on the minor version.
+    ksort($resources, SORT_NUMERIC);
+    // Get the latest resource for the minor version.
+    $resource = end($resources);
+    return array($resource['major_version'], $resource['minor_version']);
+  }
+
 }
