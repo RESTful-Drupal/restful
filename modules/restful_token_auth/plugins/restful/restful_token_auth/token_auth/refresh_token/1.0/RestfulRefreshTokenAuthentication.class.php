0ac50452a778699682d32ffc05bc0c61712b4981
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2014-11-23 19:39:42 +0100

    Issue #222: Token authentication base class.

--- modules/restful_token_auth/plugins/restful/restful_token_auth/token_auth/refresh_token/1.0/RestfulRefreshTokenAuthentication.class.php
+++ modules/restful_token_auth/plugins/restful/restful_token_auth/token_auth/refresh_token/1.0/RestfulRefreshTokenAuthentication.class.php
@@ -5,35 +5,7 @@
  * Contains RestfulRefreshTokenAuthentication.
  */
 
-class RestfulRefreshTokenAuthentication extends \RestfulEntityBase {
-
-  /**
-   * Overrides RestfulEntityBase::publicFieldsInfo().
-   *
-   * Keep only the "token" property.
-   */
-  public function publicFieldsInfo() {
-    $public_fields['access_token'] = array(
-      'property' => 'token',
-    );
-    $public_fields['type'] = array(
-      'callback' => array('\RestfulManager::echoMessage', array('Bearer')),
-    );
-    $public_fields['expires_in'] = array(
-      'property' => 'expire',
-      'process_callbacks' => array(
-        'static::intervalInSeconds',
-      ),
-    );
-    $public_fields['refresh_token'] = array(
-      'property' => 'refresh_token_reference',
-      'process_callbacks' => array(
-        'static::getTokenFromEntity',
-      ),
-    );
-
-    return $public_fields;
-  }
+class RestfulRefreshTokenAuthentication extends \RestfulTokenAuthenticationBase {
 
   /**
    * Overrides \RestfulBase::controllersInfo().
@@ -81,33 +53,6 @@ class RestfulRefreshTokenAuthentication extends \RestfulEntityBase {
     $controller = entity_get_controller($this->getEntityType());
     $token = $controller->createAccessToken($account->uid);
     return $this->viewEntity($token->id);
-  }
-
-  /**
-   * Process callback helper to get the time difference in seconds.
-   *
-   * @param int $value
-   *   The expiration timestamp in the access token.
-   *
-   * @return int
-   *   Number of seconds before expiration.
-   */
-  public static function intervalInSeconds($value) {
-    $interval = $value - time();
-    return $interval < 0 ? 0 : $interval;
-  }
-
-  /**
-   * Get the token string from the token entity.
-   *
-   * @param \RestfulTokenAuth $token
-   *   The restful_token_auth entity.
-   *
-   * @return string
-   *   The token string.
-   */
-  public static function getTokenFromEntity(\RestfulTokenAuth $token) {
-    return $token->token;
   }
 
 }
