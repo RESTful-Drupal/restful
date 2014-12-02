7241a8b10575f9b94256dafec6d4765ad2817f61
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2014-12-02 08:25:26 +0100

    Issue #224: Remove exit hook.

--- restful.api.php
+++ restful.api.php
@@ -24,29 +24,5 @@ function hook_restful_parse_request_alter(&$request) {
 }
 
 /**
- * Perform cleanup tasks.
- *
- * This hook is run at the end of most regular page requests. It is often
- * used for page logging and specialized cleanup. This hook MUST NOT print
- * anything because by the time it runs the response is already sent to
- * the browser.
- *
- * Only use this hook if your code must run even for cached page views.
- * If you have code which must run once on all non-cached pages, use
- * hook_init() instead. That is the usual case. If you implement this hook
- * and see an error like 'Call to undefined function', it is likely that
- * you are depending on the presence of a module which has not been loaded yet.
- * It is not loaded because Drupal is still in bootstrap mode.
- *
- * @see hook_exit().
- */
-function hook_restful_exit() {
-  db_update('counter')
-    ->expression('hits', 'hits + 1')
-    ->condition('type', 1)
-    ->execute();
-}
-
-/**
  * @} End of "addtogroup hooks".
  */
