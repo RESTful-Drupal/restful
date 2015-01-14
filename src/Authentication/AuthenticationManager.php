c725cebf5cee4cd0b1795dd8825324ccd1ef976f
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-01-14 13:03:53 +0100

    Do not add ALL plugins

--- src/Authentication/AuthenticationManager.php
+++ src/Authentication/AuthenticationManager.php
@@ -40,9 +40,7 @@ class AuthenticationManager implements AuthenticationManagerInterface {
    *   The authentication plugin manager.
    */
   public function __construct(AuthenticationPluginManager $manager = NULL) {
-    $manager = $manager ?: AuthenticationPluginManager::create();
-    $manager->clearCachedDefinitions();
-    $this->plugins = new AuthenticationPluginCollection($manager, $manager->getDefinitions());
+    $this->plugins = new AuthenticationPluginCollection($manager ?: AuthenticationPluginManager::create());
   }
 
   /**
@@ -66,6 +64,7 @@ class AuthenticationManager implements AuthenticationManagerInterface {
     $manager = AuthenticationPluginManager::create();
     $instance = $manager->createInstance($plugin_id);
     // The get method will instantiate a plugin if not there.
+    $this->plugins->setInstanceConfiguration($plugin_id, $manager->getDefinition($plugin_id));
     $this->plugins->set($plugin_id, $instance);
   }
 
