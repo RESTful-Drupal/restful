5d8c1797b0b72b3748c4939acdc30851dafc4366
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-01-31 09:36:43 +0100

    Scaffold new plugin type.

---
+++ src/Plugin/resource/Resource.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\restful\Plugin\Resource\Resource.
+ */
+
+namespace Drupal\restful\Plugin\Resource;
+
+use Drupal\Component\Plugin\PluginBase;
+
+abstract class Resource extends PluginBase implements ResourceInterface {
+
+  
+
+}
---
+++ src/Plugin/resource/ResourceInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\restful\Plugin\Resource\ResourceInterface.
+ */
+
+namespace Drupal\restful\Plugin\Resource;
+
+use Drupal\Component\Plugin\PluginInspectionInterface;
+
+interface ResourceInterface extends PluginInspectionInterface {}
