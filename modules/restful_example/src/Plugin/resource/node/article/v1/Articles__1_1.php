6bcd0bef2b7810847324bf5ec8f361c64737ab93
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-04-18 15:50:06 +0200

    Moved resources

---
+++ modules/restful_example/src/Plugin/resource/node/article/v1/Articles__1_1.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\restful_example\Plugin\resource\node\article\v1\Articles__1_1.
+ */
+
+namespace Drupal\restful_example\Plugin\resource\node\article\v1;
+
+use Drupal\restful\Http\RequestInterface;
+use Drupal\restful\Plugin\resource\ResourceEntity;
+use Drupal\restful\Plugin\resource\ResourceInterface;
+
+/**
+ * Class Articles
+ * @package Drupal\restful\Plugin\resource
+ *
+ * @Resource(
+ *   name = "articles:1.1",
+ *   resource = "articles",
+ *   label = "Articles",
+ *   description = "Export the article content type render cache.",
+ *   authenticationTypes = TRUE,
+ *   authenticationOptional = TRUE,
+ *   dataProvider = {
+ *     "entityType": "node",
+ *     "bundles": {
+ *       "article"
+ *     },
+ *   },
+ *   renderCache = {
+ *     "render": TRUE
+ *   },
+ *   rateLimit = {
+ *     "request": {
+ *       "event": "request",
+ *       "period": "P1D",
+ *       "limits": {
+ *         "authenticated user": 3,
+ *         "anonymous user": 2,
+ *         "administrator": -1,
+ *       }
+ *     }
+ *   },
+ *   majorVersion = 1,
+ *   minorVersion = 1
+ * )
+ */
+class Articles__1_1 extends ResourceEntity implements ResourceInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function publicFields() {
+    $public_fields = parent::publicFields();
+    $public_fields['tags'] = array(
+      'property' => 'field_tags',
+      'resource' => array(
+        'name' => 'tags',
+        'majorVersion' => 1,
+        'minorVersion' => 0,
+      ),
+    );
+    $public_fields['status'] = array(
+      'property' => 'status',
+      'methods' => array(RequestInterface::METHOD_GET),
+    );
+    $public_fields['body'] = array(
+      'property' => 'body',
+      'formatter' => array(
+        'type' => 'text_summary_or_trimmed',
+        'settings' => array(
+          'trim_length' => 100,
+        ),
+      ),
+    );
+
+    return $public_fields;
+  }
+
+  /**
+   * Helper function that adds a prefix.
+   *
+   * @param mixed $value
+   *   The input value to be prefixed.
+   * @param string $prefix
+   *   The prefix to add.
+   *
+   * @return string
+   *   The prefixed value.
+   */
+  public function addPrefix($value, $prefix) {
+    return $prefix . $value;
+  }
+
+}
---
+++ modules/restful_example/src/Plugin/resource/node/article/v2/Articles__2_0.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\restful_example\Plugin\resource\node\article\v2\Articles__1_0.
+ */
+
+namespace Drupal\restful_example\Plugin\resource\node\article\v2;
+
+use Drupal\restful\Plugin\resource\ResourceEntity;
+use Drupal\restful\Plugin\resource\ResourceInterface;
+
+/**
+ * Class Articles
+ * @package Drupal\restful\Plugin\resource
+ *
+ * @Resource(
+ *   name = "articles:2.0",
+ *   resource = "articles",
+ *   label = "Articles",
+ *   description = "Export the article content type with authentication.",
+ *   authenticationTypes = TRUE,
+ *   authenticationOptional = TRUE,
+ *   dataProvider = {
+ *     "entityType": "node",
+ *     "bundles": {
+ *       "article"
+ *     },
+ *     "viewMode": {
+ *       "name": "default",
+ *       "fieldMap": {
+ *         "body": "body",
+ *         "field_tags": "tags",
+ *         "field_image": "image",
+ *       }
+ *     }
+ *   },
+ *   majorVersion = 2,
+ *   minorVersion = 0
+ * )
+ */
+class Articles__2_0 extends ResourceEntity implements ResourceInterface {}
