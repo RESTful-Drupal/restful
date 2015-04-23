e8a79ede5f8f8b8b60d95b4165bd9e6bd6314ec4
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-04-23 08:56:14 +0200

    Implementing peer review feedback

--- src/RestfulManager.php
+++ src/RestfulManager.php
@@ -188,10 +188,10 @@ class RestfulManager {
    *
    * @param DataInterpreterInterface $value
    *   The resource value.
-   * @param string $message
+   * @param mixed $message
    *   The string to relay.
    *
-   * @return string
+   * @return mixed
    *   Returns $message
    */
   public static function echoMessage(DataInterpreterInterface $value, $message) {
