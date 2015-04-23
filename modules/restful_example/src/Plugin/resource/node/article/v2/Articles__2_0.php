ff7f9a265382b22e540d181b1c943e51d6d6e3d1
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-04-23 14:57:32 +0200

    Version negotiation test

--- modules/restful_example/src/Plugin/resource/node/article/v2/Articles__2_0.php
+++ modules/restful_example/src/Plugin/resource/node/article/v2/Articles__2_0.php
@@ -2,13 +2,15 @@
 
 /**
  * @file
- * Contains \Drupal\restful_example\Plugin\resource\node\article\v2\Articles__1_0.
+ * Contains \Drupal\restful_example\Plugin\resource\node\article\v2\Articles__2_0.
  */
 
 namespace Drupal\restful_example\Plugin\resource\node\article\v2;
 
+use Drupal\restful\Http\RequestInterface;
 use Drupal\restful\Plugin\resource\ResourceEntity;
 use Drupal\restful\Plugin\resource\ResourceInterface;
+use Drupal\restful\Plugin\resource\ResourceNode;
 
 /**
  * Class Articles
@@ -18,25 +20,16 @@ use Drupal\restful\Plugin\resource\ResourceInterface;
  *   name = "articles:2.0",
  *   resource = "articles",
  *   label = "Articles",
- *   description = "Export the article content type with authentication.",
- *   authenticationTypes = TRUE,
+ *   description = "Export the article content type.",
  *   authenticationOptional = TRUE,
  *   dataProvider = {
  *     "entityType": "node",
  *     "bundles": {
  *       "article"
  *     },
- *     "viewMode": {
- *       "name": "default",
- *       "fieldMap": {
- *         "body": "body",
- *         "field_tags": "tags",
- *         "field_image": "image",
- *       }
- *     }
  *   },
  *   majorVersion = 2,
  *   minorVersion = 0
  * )
  */
-class Articles__2_0 extends ResourceEntity implements ResourceInterface {}
+class Articles__2_0 extends ResourceNode implements ResourceInterface {}
