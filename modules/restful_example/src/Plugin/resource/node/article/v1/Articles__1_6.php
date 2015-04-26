d1516e486961cc8bcf2d17756ac848a695ea5658
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-04-26 12:52:53 +0200

    Create resource Articles__1_6

---
+++ modules/restful_example/src/Plugin/resource/node/article/v1/Articles__1_6.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\restful_example\Plugin\resource\node\article\v1\Articles__1_0.
+ */
+
+namespace Drupal\restful_example\Plugin\resource\node\article\v1;
+
+use Drupal\restful\Http\RequestInterface;
+use Drupal\restful\Plugin\resource\ResourceEntity;
+use Drupal\restful\Plugin\resource\ResourceInterface;
+use Drupal\restful\Plugin\resource\ResourceNode;
+
+/**
+ * Class Articles
+ * @package Drupal\restful\Plugin\resource
+ *
+ * @Resource(
+ *   name = "articles:1.6",
+ *   resource = "articles",
+ *   label = "Articles",
+ *   description = "Export the articles with all authentication providers.",
+ *   authenticationTypes = TRUE,
+ *   authenticationOptional = TRUE,
+ *   dataProvider = {
+ *     "entityType": "node",
+ *     "bundles": {
+ *       "article"
+ *     },
+ *   },
+ *   formatter = "hal_xml",
+ *   majorVersion = 1,
+ *   minorVersion = 6
+ * )
+ */
+class Articles__1_6 extends ResourceNode implements ResourceInterface {
+
+  /**
+   * Overrides ResourceNode::publicFields().
+   */
+  protected function publicFields() {
+    $public_fields = parent::publicFields();
+
+    $public_fields['body'] = array(
+      'property' => 'body',
+      'sub_property' => 'value',
+    );
+
+    return $public_fields;
+  }
+
+
+}
