69a48051a202d13d10994a3001839546eb9f0d32
Roy Segall <roy.segall@gizra.com>
2015-07-15 09:43:58 +0300

    Starting test sub requests.

--- tests/modules/restful_test/plugins/restful/taxonomy_term/test_tags/1.0/RestfulEntityTaxonomyTermTestTags.class.php
+++ tests/modules/restful_test/plugins/restful/taxonomy_term/test_tags/1.0/RestfulEntityTaxonomyTermTestTags.class.php
@@ -6,16 +6,4 @@
  */
 
 class RestfulEntityTaxonomyTermTestTags extends \RestfulEntityBaseTaxonomyTerm {
-
-  /**
-   * Overrides \RestfulEntityBaseTaxonomyTerm::checkEntityAccess().
-   *
-   * Allow access to create "Tags" resource for privileged users, as
-   * we can't use entity_access() since entity_metadata_taxonomy_access()
-   * denies it for a non-admin user.
-   */
-  protected function checkEntityAccess($op, $entity_type, $entity) {
-    $account = $this->getAccount();
-    return parent::checkEntityAccess($op, $entity_type, $entity) || user_access('create article content', $account);
-  }
 }
