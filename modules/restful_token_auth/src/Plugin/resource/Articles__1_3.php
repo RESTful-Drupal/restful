d40d52c604a9a20ace814a666ebe12dfa9559893
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-11-13 06:09:22 -0500

    Moved resource to the example module

---
+++ modules/restful_token_auth/src/Plugin/resource/Articles__1_3.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\restful_token_auth\Plugin\resource\Articles__1_3.
+ */
+
+namespace Drupal\restful_token_auth\Plugin\resource;
+
+use Drupal\restful\Plugin\resource\ResourceInterface;
+use Drupal\restful\Plugin\resource\ResourceNode;
+
+/**
+ * Class Articles__1_3
+ * @package Drupal\restful\Plugin\resource
+ *
+ * @Resource(
+ *   name = "articles:1.3",
+ *   resource = "articles",
+ *   label = "Articles",
+ *   description = "Export the articles with all authentication providers.",
+ *   authenticationTypes = {
+ *     "token"
+ *   },
+ *   dataProvider = {
+ *     "entityType": "node",
+ *     "bundles": {
+ *       "article"
+ *     },
+ *   },
+ *   majorVersion = 1,
+ *   minorVersion = 3
+ * )
+ */
+class Articles__1_3 extends ResourceNode implements ResourceInterface {}
