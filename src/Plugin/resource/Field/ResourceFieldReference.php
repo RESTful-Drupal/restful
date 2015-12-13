ae17134d9c5e43f90f6c103c5f1c9a52815f4a02
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-12-13 06:57:08 -0500

    Expanded docs

--- src/Plugin/resource/Field/ResourceFieldReference.php
+++ src/Plugin/resource/Field/ResourceFieldReference.php
@@ -17,6 +17,13 @@ use Drupal\restful\Plugin\resource\DataInterpreter\DataInterpreterInterface;
  * field type will allow you to have a field definition with a callback return
  * an ID and use that as a relationship.
  *
+ * This is specially useful when adding a relationship to an entity based
+ * resource from a DB query, or vice versa. See an example of this in action in
+ * the example resource main:1.8.
+ *
+ * If you need to add a reference to entity things like $node->uid, use
+ * \Drupal\restful\Plugin\resource\Field\ResourceFieldEntityReference instead.
+ *
  * @package Drupal\restful\Plugin\resource\Field
  */
 class ResourceFieldReference extends ResourceField {
