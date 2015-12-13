a24c2b58900cbe742125337b1174b26041b77ccc
Theodoros Ploumis <me@theodorosploumis.com>
2015-12-13 05:19:52 -0500

    [CLEANUP] Fixed c&p description in Users resource
    It was showing the articles resource instead.

--- src/Plugin/resource/Users__1_0.php
+++ src/Plugin/resource/Users__1_0.php
@@ -15,7 +15,7 @@ namespace Drupal\restful\Plugin\resource;
  *   name = "users:1.0",
  *   resource = "users",
  *   label = "Users",
- *   description = "Export the article content type with cookie authentication.",
+ *   description = "Export the User entity with cookie authentication.",
  *   authenticationTypes = TRUE,
  *   authenticationOptional = TRUE,
  *   dataProvider = {
