3a38f64dfbef0af41b874126db4535d0db3ef972
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2016-02-22 14:38:56 +0100

    [FEATURE] Allow other auth providers to use Authorization
    Let other providers to use the Authorization header. The Basic
    prefix will be used to detect basic authentication when that
    header is used. This allows Simple OAuth.
    
    Additionally changes the String class name, since it's not
    compatible with PHP7.

---
+++ src/Util/StringHelper.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\restful\Util\String.
+ */
+
+namespace Drupal\restful\Util;
+
+class StringHelper {
+
+  /**
+   * Turns a string into camel case. From search_api_index to SearchApiIndex.
+   *
+   * @param string $input
+   *   The input string.
+   *
+   * @return string
+   *   The camelized string.
+   */
+  public static function camelize($input) {
+    $input = preg_replace('/[-_]/', ' ', $input);
+    $input = ucwords($input);
+    $parts = explode(' ', $input);
+    return implode('', $parts);
+  }
+
+  /**
+   * Remove the string prefix from the token value.
+   *
+   * @param string $prefix
+   *   The prefix to remove.
+   * @param string $haystack
+   *   The string to remove the prefix from.
+   *
+   * @return string
+   *   The prefixless string. NULL if the prefix is not found.
+   */
+  public static function removePrefix($prefix, $haystack) {
+    $position = strpos($haystack, $prefix);
+    if ($position === FALSE) {
+      return NULL;
+    }
+    return substr($haystack, $position + strlen($prefix));
+  }
+
+}
