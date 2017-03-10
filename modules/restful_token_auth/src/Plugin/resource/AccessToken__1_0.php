d9c1b060b358d9980e6d31d1645575f24bb90419
Tom Nightingale <tom@tnightingale.com>
2017-03-10 13:40:15 -0800

    Re-enabling expired token cleanup when creating a new access token. Removing code that deletes a user's extra refresh tokens.

--- modules/restful_token_auth/src/Plugin/resource/AccessToken__1_0.php
+++ modules/restful_token_auth/src/Plugin/resource/AccessToken__1_0.php
@@ -63,31 +63,25 @@ class AccessToken__1_0 extends TokenAuthenticationBase implements ResourceInterf
 
     // Check if there is a token that did not expire yet.
     /* @var DataProviderEntityInterface $data_provider */
-    // $data_provider = $this->getDataProvider();
-    // $query = $data_provider->EFQObject();
-    // $result = $query
-    //   ->entityCondition('entity_type', $entity_type)
-    //   ->entityCondition('bundle', 'access_token')
-    //   ->propertyCondition('uid', $account->uid)
-    //   ->range(0, 1)
-    //   ->execute();
-    //
-    // $token_exists = FALSE;
-    //
-    // if (!empty($result[$entity_type])) {
-    //   $id = key($result[$entity_type]);
-    //   $access_token = entity_load_single($entity_type, $id);
-    //
-    //   $token_exists = TRUE;
-    //   if (!empty($access_token->expire) && $access_token->expire < REQUEST_TIME) {
-    //     if (variable_get('restful_token_auth_delete_expired_tokens', TRUE)) {
-    //       // Token has expired, so we can delete this token.
-    //       $access_token->delete();
-    //     }
-    //
-    //     $token_exists = FALSE;
-    //   }
-    // }
+    $data_provider = $this->getDataProvider();
+    $query = $data_provider->EFQObject();
+    $result = $query
+      ->entityCondition('entity_type', $entity_type)
+      ->entityCondition('bundle', 'access_token')
+      ->propertyCondition('uid', $account->uid)
+      ->execute();
+
+    if (!empty($result[$entity_type])) {
+      foreach ($result[$entity_type] as $id => $value) {
+        $access_token = entity_load_single($entity_type, $id);
+        if (!empty($access_token->expire) && $access_token->expire < REQUEST_TIME) {
+          if (variable_get('restful_token_auth_delete_expired_tokens', TRUE)) {
+            // Token has expired, so we can delete this token.
+            $access_token->delete();
+          }
+        }
+      }
+    }
 
     /* @var \Drupal\restful_token_auth\Entity\RestfulTokenAuthController $controller */
     $controller = entity_get_controller($this->getEntityType());
